862a19c3a2091276c323d8285617f0a669444525
Daeseong Kim <16497919+dkimds@users.noreply.github.com>
2024-12-24 15:30:58 +0900

    Move files

--- part2/pipeline_train.py
+++ part2/pipeline_train.py
@@ -1,14 +1,17 @@
+# pipeline_train.py
+import joblib
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-import joblib
 
-# get data
+# 1. get data
 X, y = load_iris(return_X_y=True, as_frame=True)
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2022)
 
-# train model
+# 2. model development and train
 model_pipeline = Pipeline([("scaler", StandardScaler()), ("svc", SVC())])
 model_pipeline.fit(X_train, y_train)
 
@@ -21,5 +24,5 @@ valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
 print("Train Accuracy :", train_acc)
 print("Valid Accuracy :", valid_acc)
 
-# save model
+# 3. save model
 joblib.dump(model_pipeline, "model_pipeline.joblib")
