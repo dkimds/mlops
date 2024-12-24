0c0ceb158912ee1a4ea26d6d4fbf75c409d374ce
Daeseong Kim <16497919+dkimds@users.noreply.github.com>
2024-12-24 15:06:13 +0900

    Create a model pipeline

---
+++ pipeline_train.py
@@ -0,0 +1,25 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import joblib
+
+# get data
+X, y = load_iris(return_X_y=True, as_frame=True)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2022)
+
+# train model
+model_pipeline = Pipeline([("scaler", StandardScaler()), ("svc", SVC())])
+model_pipeline.fit(X_train, y_train)
+
+train_pred = model_pipeline.predict(X_train)
+valid_pred = model_pipeline.predict(X_valid)
+
+train_acc = accuracy_score(y_true=y_train, y_pred=train_pred)
+valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
+
+print("Train Accuracy :", train_acc)
+print("Valid Accuracy :", valid_acc)
+
+# save model
+joblib.dump(model_pipeline, "model_pipeline.joblib")
